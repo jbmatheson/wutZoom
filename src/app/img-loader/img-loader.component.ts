7f5d3b0f117ac2a320eb94186a01b6671ba55dab
Jim B. Matheson <jimbmatheson@gmail.com>
2017-12-03 21:14:01 -0500

    added load image functionality and click-to-crop

--- src/app/img-loader/img-loader.component.ts
+++ src/app/img-loader/img-loader.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-img-loader',
   templateUrl: './img-loader.component.html',
   styleUrls: ['./img-loader.component.css']
 })
-export class ImgLoaderComponent implements OnInit {
+export class ImgLoaderComponent implements AfterViewInit {
+  @ViewChild('myFile') myFile: ElementRef;
+
+  imgURL = 'assets/dragon.jpg';
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
   }
 
   add() {
-    var r = new FileReader();
-    console.log("LOADED");
+    this.imgURL = this.myFile.nativeElement.value; 
+  }
+
+  change(){ }
+
+  snip($e){
+    var x = $e.clientX;
+    var y = $e.clientY;
+    console.log("Xpos " + x + " | Ypos: " + y);
+    var myImage = new Image(200, 200);
+    myImage.src = this.imgURL;
+    document.body.appendChild(myImage);
   }
 }
