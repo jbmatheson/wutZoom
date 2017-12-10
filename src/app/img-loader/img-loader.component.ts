19f740ba54cbf203396d7ba9dcefa71103ef68dd
Jim B. Matheson <jimbmatheson@gmail.com>
2017-12-09 21:59:24 -0500

    WIP - cropping image

--- src/app/img-loader/img-loader.component.ts
+++ src/app/img-loader/img-loader.component.ts
@@ -21,12 +21,20 @@ export class ImgLoaderComponent implements AfterViewInit {
 
   change(){ }
 
-  snip($e){
-    var x = $e.clientX;
-    var y = $e.clientY;
-    console.log("Xpos " + x + " | Ypos: " + y);
-    var myImage = new Image(400, 400);
-    myImage.src = this.imgURL;
-    document.body.appendChild(myImage);
+  snip(evt){  
+    var crop_canvas,
+      myImage = document.getElementById("img-01"),
+      width = 200,
+      height = 200;
+
+    crop_canvas = document.createElement('canvas');
+    crop_canvas.width = width;
+    crop_canvas.height = height;
+
+    //myImage.src = this.imgURL;
+    crop_canvas.getContext('2d').drawImage(myImage, evt.offsetX, evt.offsetY, 150, 150, 0, 0, 150, 150);
+
+    document.body.appendChild(crop_canvas);
+    console.log("Xpos: " + evt.offsetX + " | Ypos: " + evt.offsetY);
   }
 }
